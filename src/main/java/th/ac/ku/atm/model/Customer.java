57b5530e0234060241ab27e11ae6da8865d2fa1b
326th <minecraftskinman@gmail.com>
2020-10-05 16:58:20 +0700

    change to connect to jpa

--- src/main/java/th/ac/ku/atm/model/Customer.java
+++ src/main/java/th/ac/ku/atm/model/Customer.java
@@ -1,15 +1,16 @@
 package th.ac.ku.atm.model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Customer {
+
+    @Id
     private int id;
     private String name;
     private String pin;
 
-    public Customer(int id, String name, String pin) {
-        this.id = id;
-        this.name = name;
-        this.pin = pin;
-    }
+    public Customer() {}
 
     public int getId() {
         return id;
